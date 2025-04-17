6d1cd98159d916f5bc78e17a8f0b1aa306c84a8a
Vlad <v.v.kovba@gmail.com>
2025-04-17 13:19:26 +0300

    Добавьте файлы проекта.

---
+++ OOP_Project_Kovba/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OOP_Project_Kovba.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
